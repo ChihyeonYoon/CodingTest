54c791291fe4dcf94fff1e849fb2d6b817a5c622
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-03-06 16:09:05 +0900

    [level 1] Title: 덧칠하기, Time: 5.62 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,13 @@
+def solution(n, m, section):
+
+    answer = 0
+    start = section[0]
+
+    for i in range(len(section)):
+        if section[i] - start >=m:
+            answer +=1
+            start = section[i]
+        
+
+    
+    return answer+1
