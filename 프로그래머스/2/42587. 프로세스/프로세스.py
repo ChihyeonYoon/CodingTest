36866984b9f41f8b44fa8785714dd42d5b40805c
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-03-07 01:16:29 +0900

    [level 2] Title: 프로세스, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,21 @@
+def solution(priorities, location):
+    answer = 0
+    
+    priorities = [(p,i) for i, p in enumerate(priorities)]
+    print(priorities)
+
+    while priorities:
+        mp = max([p for p,_ in priorities])
+        
+        while priorities[0][0] != mp:
+            tmp = priorities.pop(0)
+            priorities.append(tmp)
+        # print(priorities)
+        # break
+        if priorities[0][0] == mp:
+            p, l = priorities.pop(0)
+            answer += 1
+            if l == location:
+                break
+
+    return answer
