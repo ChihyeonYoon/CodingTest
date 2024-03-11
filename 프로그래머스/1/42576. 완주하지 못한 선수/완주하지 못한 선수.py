e65910adebf05c7e6a93d244b2c6d1175b9ce225
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-03-12 02:05:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.24 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,4 +1,6 @@
 def solution(participant:list, completion:list):
+    # participant = {p:participant.count(p) for p in participant}
+    # completion = {c:completion.count(c) for c in completion}
     def to_table(l:list):
         table = {x:0 for x in l}
         for x in l:
