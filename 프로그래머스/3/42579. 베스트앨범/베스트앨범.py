c3cfaa8f64577ac89046db5c0f1a2c3be3b21939
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-03-12 19:05:10 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,37 @@
+def solution(genres, plays):
+    answer = []
+
+    i_g_p = [(i,g,p) for i, (g, p) in enumerate(zip(genres,plays))]
+    i_g_p.sort(key=lambda x: x[2], reverse= True)
+    print(i_g_p)
+
+    table = {}
+    table_ = {}
+    table_oder =[]
+    for i, g, p in i_g_p:
+        if g not in table:
+            table[g] = [i]
+            table_[g] = p
+            
+        elif g in table and len(table[g])<2:
+            table[g].append(i)
+            table_[g] += p
+        
+        else:
+            table_[g] += p
+
+    for k, i in table_.items():
+        table_oder.append((k,i))
+    
+    table_oder.sort(key=lambda x: x[1],reverse=True)
+
+    print(table)
+    print(table_)
+
+    for t, p in table_oder:
+        answer += table[t]
+
+    
+
+    print(answer)
+    return answer
