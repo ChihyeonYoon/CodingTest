5d35aaae359ad70d9b351775aea4e61d0e5f75cd
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-03-18 17:07:47 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,3 +1,31 @@
+# def solution(begin, target, words):
+
+#     def CanChange(s,t):
+#         diff = 0
+#         for s_ , t_ in zip(s,t):
+#             if s_ != t_:
+#                 diff +=1
+#         if diff == 1:
+#             return True
+#         else: return False
+
+#     if target not in words:
+#         return 0
+
+#     from collections import deque
+#     q = deque()
+#     q.append((begin, 0))
+
+#     while q:
+#         cur, dep = q.popleft()
+
+#         if cur == target:
+#             return dep
+        
+#         for w in words:
+#             if CanChange(cur,w):
+#                 q.append((w,dep+1))
+
 def solution(begin, target, words):
 
     def CanChange(s,t):
@@ -35,8 +63,6 @@ def solution(begin, target, words):
 
                 elif w not in visited:
                     dfs(w,dep+1)
-
-
 
     dfs(begin,0)
     return min(answers)
