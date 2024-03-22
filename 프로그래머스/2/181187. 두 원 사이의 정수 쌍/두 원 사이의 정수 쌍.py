e713830213b0c108fb7a748b0df69c327036479a
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-03-22 14:52:22 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 1543.59 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,14 @@
+def solution(r1, r2):
+    counter = 0
+    y_min, y_max = r1, r2
+    
+    for x in range(0,r2):
+        while r2**2 < y_max**2 + x**2:
+            y_max -=1
+        
+        while y_min-1 and r1**2 <= (y_min-1)**2 + x**2:
+            y_min-=1
+
+        counter += y_max-y_min+1
+    
+    return counter*4
