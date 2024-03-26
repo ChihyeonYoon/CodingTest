a6dd1bd0a1f34e99c77f9e4a19ebc910830c7fe1
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-03-26 20:35:44 +0900

    [level 2] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -53,3 +53,61 @@ def solution(maps):
         
    	# 둘중 하나라도 -1 이면 탈출할 수 없음
     return -1
+
+# from collections import deque
+# def bfs(start_point, end_point, maps):
+#         y_len = len(maps)-1
+#         x_len = len(maps[0])-1
+#         direction = [(1,0),(0,1),(0,-1),(-1,0)]
+
+#         start_y, start_x = start_point
+#         q = deque([(start_y, start_x, 0)]) #(y, x, dist)
+        
+#         visited = {}
+#         # visited_map = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+
+#         while q:
+#             cur_y, cur_x, cur_dist = q.popleft()
+
+#             if (cur_y, cur_x) == end_point:
+#                 return cur_dist
+#             else:
+#                 visited[(cur_y, cur_x)]=cur_dist
+#                 # visited_map[cur_y][cur_x]=True
+            
+#             for y, x in direction:
+#                 next_y, next_x = cur_y+y, cur_x+x
+#                 # if 0 <= next_y <= y_len and 0 <= next_x <= x_len and visited_map[next_y][next_x]==False:
+#                 if 0 <= next_y <= y_len and 0 <= next_x <= x_len and maps[next_y][next_x] != 'X' :
+#                     if (next_y, next_x) not in visited:
+                        
+#                         q.append((next_y, next_x, cur_dist+1))
+#                         # visited[(cur_y, cur_x)]=cur_dist
+#                         # visited_map[cur_y][cur_x]=True
+#         return None
+
+
+# def solution(maps):
+#     start_point, lever_point, end_point, flag = None,  None,  None, False
+
+#     for y, row in enumerate(maps):
+#         for x, s in enumerate(row):
+#             if s == 'S':
+#                 start_point = (y,x)
+#             if s == 'L':
+#                 lever_point = (y,x)
+#             if s == 'E':
+#                 end_point = (y,x)
+#             if start_point and lever_point and end_point:
+#                 flag = True
+#         if flag:
+#             break
+
+#     dist_s2l = bfs(start_point, lever_point, maps)
+        
+#     dist_l2e = bfs(lever_point, end_point, maps)
+
+#     if dist_s2l == None or dist_l2e == None:
+#         return -1
+#     else:
+#         return dist_s2l+ dist_l2e
