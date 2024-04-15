9914f800d418c57f6cb5770c151615e5d34dc6e4
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-04-16 01:15:53 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,30 @@
+def solution(n, lost, reserve):
+    answer = 0
+    reserve_dict = {}
+    for r in reserve:
+        if r not in lost:
+            reserve_dict[r] = 2
+        else :
+            reserve_dict[r] = 1
+    print(reserve_dict)
+    
+    # for l in lost:
+    for l in range(1,n+1):
+        if l in lost:
+            if l-1 in reserve_dict and reserve_dict[l-1] ==2:
+                reserve_dict[l-1] -= 1
+                answer +=1
+            elif l+1 in reserve_dict and reserve_dict[l+1] ==2:
+                reserve_dict[l+1]-=1
+                answer +=1
+            elif l in reserve_dict and reserve_dict[l] == 1:
+                answer +=1
+        else:
+            answer+=1
+    print(reserve_dict)
+    # answer +=len(reserve)
+            
+    
+    
+            
+    return answer
