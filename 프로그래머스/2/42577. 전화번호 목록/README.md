8d78f5a0dfa5d57600990a272013fd57ec27ed78
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-05-20 16:12:26 +0900

    [level 2] Title: 전화번호 목록, Time: 385.05 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 34.6 MB, 시간: 319.60 ms
+메모리: 31.3 MB, 시간: 385.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 12일 15:12:00
+2024년 05월 20일 16:12:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,21 +1,12 @@
-# def solution(phone_book:list):
-#     phone_book.sort(key=lambda x: len(x))
-#     # table = {t:True for t in phone_book}
-#     from collections import deque
-#     phone_book = deque(phone_book)
-#     while phone_book:
-#         p = phone_book.popleft()
-#         if any(t.startswith(p) and t!= p for t in phone_book):
-#             return False
-#     return True
-
 def solution(phone_book:list):
-    table = {p:True for p in phone_book}
-
+    # table = {p:True for p in phone_book}
+    s = set()
+    phone_book.sort()
     for p in phone_book:
-        prepix = ''
+        prefix = ''
         for n in p:
-            prepix += n
-            if prepix in table and prepix != p:
+            prefix += n
+            if s.__contains__(prefix):
                 return False
+        s.add(p)
     return True
