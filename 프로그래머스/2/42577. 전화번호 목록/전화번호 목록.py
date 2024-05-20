9f915fade3fd344de51406fe13fbd8737789ce46
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-05-20 21:17:44 +0900

    [level 2] Title: 전화번호 목록, Time: 429.14 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -10,3 +10,37 @@ def solution(phone_book:list):
                 return False
         s.add(p)
     return True
+
+# def solution(phone_book:list):
+#     table = {p:True for p in phone_book}
+
+#     for p in phone_book:
+#         prepix = ''
+#         for n in p:
+#             prepix += n
+#             if prepix in table and prepix != p:
+#                 return False
+#     return True
+
+# def solution(phone_book):
+#     prefixs = sorted(phone_book)
+#     # print(prefixs)
+    
+#     dict = {}
+#     for num in prefixs:
+#         if len(num) not in dict:
+#             dict[len(num)] = []
+#             dict[len(num)].append(num)
+#         else:
+#             dict[len(num)].append(num)
+            
+#     # print(dict)
+            
+#     for prefix in prefixs:
+#         for key in dict.keys():
+#             if key > len(prefix):
+#                 for number in dict[key]:
+#                     if number[:len(prefix)] == prefix:
+#                         return False         
+        
+#     return True
