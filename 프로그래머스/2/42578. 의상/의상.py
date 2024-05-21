10a3a89569e0fb3561f0939f0c65ee9263a6ef5b
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-05-21 15:25:46 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,13 @@
 def solution(clothes):
     answer = 1
-    table = {t:[] for i,t in clothes}
+    table = {t:[] for c,t in clothes}
     
-    for i, t in clothes:
-        table[t].append(i)
-        
+    for c, t in clothes:
+        table[t].append(c)
+    
+    print(table)
+    
     for v in table.values():
-        answer *=(len(v)+1)
+        answer *=(len(v)+1) # len(v)+1 : 해당 종류의 옷을 선택 안하는 경우를 추가
     
-    return answer -1
+    return answer -1 # 아무거도 안입는 경우의 수를 빼줌
