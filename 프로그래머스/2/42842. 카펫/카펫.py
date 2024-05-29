f19052419d6cdcd3414246602fa54cd9d58028bd
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-05-29 16:00:34 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,16 +1,11 @@
 def solution(brown, yellow):
-    answer = []
     carpet_size = brown+yellow
-    pair_diff = carpet_size - 1
-    
-    
+      
     for d in range(2, carpet_size-1):
         t, r = divmod(carpet_size,d)
-        # print(d,t,r)
         
         if r == 0:
             pair = [max(d,t),min(d,t)]
-            y_region = [max(d,t)-1,min(d,t)-1]
             if (max(d,t)-2)*(min(d,t)-2) == yellow:
                 return pair
     
