9444e8f709fb1772b80b7d378d976e4b881bbacc
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-06-07 19:43:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.55 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -27,3 +27,42 @@ def solution(maps):
     result = bfs(maps, (0,0)) # y, x
     print(result)
     return result[(len(maps)-1, len(maps[0])-1)] +1 if (len(maps)-1, len(maps[0])-1) in result else -1
+
+def solution(maps):
+    answer = []
+
+    ver_len = len(maps)
+    horiz_len = len(maps[0])
+
+    from collections import deque
+    visited = {}
+    q = deque()
+    q.append((0,0,1))
+
+    while q:
+        cur = q.popleft()
+        x,y,a = cur
+        
+        if (x,y) in visited:
+            continue
+
+        if y<0 or y>ver_len-1 or x<0 or x>horiz_len-1:
+            continue
+        
+        if maps[y][x] == 0:
+            continue
+        
+        if maps[y][x] == 1:
+            print(x,y,':',a)
+            # answer += 1
+            visited[(x,y)]=a
+            if y == ver_len-1 and x == horiz_len-1:
+                print("End Point")
+                continue
+
+            q.append((x+1,y,a+1))#right_pt
+            q.append((x-1,y,a+1))#left_pt 
+            q.append((x,y+1,a+1))#down_pt 
+            q.append((x,y-1,a+1))#up_pt 
+
+    return visited[(horiz_len-1, ver_len-1)] if (horiz_len-1, ver_len-1) in visited else -1
