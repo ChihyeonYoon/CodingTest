0fee1cab778f887dc1111bc378dc93e165b02790
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-06-10 09:32:11 +0900

    Update 조이스틱.py

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -20,6 +20,51 @@ def solution(name):
     answer += min_move
     return answer
 
-# 예시 입력
-print(solution("JEROEN"))  # 56
-print(solution("JAN"))     # 23
+# AABAAABAA
+# 2
+# i  0
+#         next_idx:  2
+#         n - next_idx = 9 - 2 = 7
+#         distance = min(i, n - next_idx) = min(0, 9 - 2) = 0
+#         min_move = min(min_move, i + n - next_idx + distance) = min(8, 0 + 9 - 2 + 0) = 7
+# i  1
+#         next_idx:  2
+#         n - next_idx = 9 - 2 = 7
+#         distance = min(i, n - next_idx) = min(1, 9 - 2) = 1
+#         min_move = min(min_move, i + n - next_idx + distance) = min(7, 1 + 9 - 2 + 1) = 7
+# i  2
+#         next_idx:  6
+#         n - next_idx = 9 - 6 = 3
+#         distance = min(i, n - next_idx) = min(2, 9 - 6) = 2
+#         min_move = min(min_move, i + n - next_idx + distance) = min(7, 2 + 9 - 6 + 2) = 7
+# i  3
+#         next_idx:  6
+#         n - next_idx = 9 - 6 = 3
+#         distance = min(i, n - next_idx) = min(3, 9 - 6) = 3
+#         min_move = min(min_move, i + n - next_idx + distance) = min(7, 3 + 9 - 6 + 3) = 7
+# i  4
+#         next_idx:  6
+#         n - next_idx = 9 - 6 = 3
+#         distance = min(i, n - next_idx) = min(4, 9 - 6) = 3
+#         min_move = min(min_move, i + n - next_idx + distance) = min(7, 4 + 9 - 6 + 3) = 7
+# i  5
+#         next_idx:  6
+#         n - next_idx = 9 - 6 = 3
+#         distance = min(i, n - next_idx) = min(5, 9 - 6) = 3
+#         min_move = min(min_move, i + n - next_idx + distance) = min(7, 5 + 9 - 6 + 3) = 7
+# i  6
+#         next_idx:  9
+#         n - next_idx = 9 - 9 = 0
+#         distance = min(i, n - next_idx) = min(6, 9 - 9) = 0
+#         min_move = min(min_move, i + n - next_idx + distance) = min(7, 6 + 9 - 9 + 0) = 6
+# i  7
+#         next_idx:  9
+#         n - next_idx = 9 - 9 = 0
+#         distance = min(i, n - next_idx) = min(7, 9 - 9) = 0
+#         min_move = min(min_move, i + n - next_idx + distance) = min(6, 7 + 9 - 9 + 0) = 6
+# i  8
+#         next_idx:  9
+#         n - next_idx = 9 - 9 = 0
+#         distance = min(i, n - next_idx) = min(8, 9 - 9) = 0
+#         min_move = min(min_move, i + n - next_idx + distance) = min(6, 8 + 9 - 9 + 0) = 6
+# 8
