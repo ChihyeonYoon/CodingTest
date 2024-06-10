7016ed2d885d568cc5c635c8ab62ee99efa60e90
ChihyeonYoon <82532703+ChihyeonYoon@users.noreply.github.com>
2024-06-10 10:48:27 +0900

    [level 2] Title: 구명보트, Time: 12.96 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,17 +1,44 @@
+# 50 50 70 80
+# XX XX 70 80
+# XX XX XX 80
+# XX XX XX XX
+
+# 70 50 80 30 -> 30 50 70 80
+# XX 50 80 XX    XX 50 XX 80
+# XX XX 80 XX    XX 50 XX 80
+# XX XX XX XX    XX XX XX XX
+
 def solution(people, limit):
     answer = 0
-    # L        H
+    l,r = 0, len(people)-1
+    
+    
+    boat = 0
     people.sort()
-    Lidx, Hidx = 0, len(people)-1
-    while Lidx < Hidx:
-        # 2 go (H + L)
-        if people[Lidx] + people[Hidx] <= limit:
-            Lidx +=1
-            Hidx -=1
-        # 1 go (H)
-        else:
-            Hidx -=1
-        answer +=1
-    if Hidx == Lidx:
-        answer +=1
+    # print(people)
+    
+    gone = set()
+    
+    while l<r:
+        # print(l,r)
+#         print(people[l], people[r])
+            
+        if people[l]+people[r] <= limit:
+            # print(f"\t{people[l]}+{people[r]} <= {limit}")
+            answer +=1
+            gone.add(l)
+            gone.add(r)
+            l+=1
+            r-=1
+            
+            continue
+            
+        elif people[l]+people[r] > limit:
+            r-=1
+        
+    # print(gone)
+    for i,_ in enumerate(people):
+        if i not in gone:
+            answer +=1
+            
     return answer
